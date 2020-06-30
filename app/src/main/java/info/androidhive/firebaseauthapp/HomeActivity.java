0d9c4b6c0a5c27a157bdcdce79aa6674ae0a8eee
Bob_jr <shendavid920@gmail.com>
2020-06-30 17:43:14 +0800

    Update HomeActivity.java

--- app/src/main/java/info/androidhive/firebaseauthapp/HomeActivity.java
+++ app/src/main/java/info/androidhive/firebaseauthapp/HomeActivity.java
@@ -12,15 +12,6 @@ import android.view.KeyEvent;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import java.util.HashMap;
-import java.util.Map;
 
 import info.androidhive.firebaseauthapp.ui.dashboard.DashboardFragment;
 import info.androidhive.firebaseauthapp.ui.home.HomeFragment;
